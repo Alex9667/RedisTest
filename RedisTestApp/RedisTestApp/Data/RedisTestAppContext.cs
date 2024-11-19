b872b3b9d2bcbb0e46f99c6f9f461104fe136669
Alexander <blablabla2013@live.dk>
2024-11-19 23:15:50 +0100

    Added mvc project and redis nuget package

---
+++ RedisTestApp/RedisTestApp/Data/RedisTestAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RedisTestApp.Models;
+
+namespace RedisTestApp.Data
+{
+    public class RedisTestAppContext : DbContext
+    {
+        public RedisTestAppContext (DbContextOptions<RedisTestAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RedisTestApp.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ RedisTestApp/RedisTestApp/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace RedisTestApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public required string Color { get; set; }
+    }
+}
